3e28b2feea99d2b3b6e26bc147dc4500582dfbc9
Kazi Rakin Bin Zaman <segfaulted11@gmail.com>
2026-06-17 09:39:36 +0000

    changed marquee

--- dragon-news/src/components/shared/Marquee.jsx
+++ dragon-news/src/components/shared/Marquee.jsx
@@ -1,12 +1,20 @@
+import { getNewsByCategoryId } from "@/lib/data";
 import Marquee from "react-fast-marquee";
 
-const Marqueee = () => {
+const Marqueee = async() => {
+    const breakingNews = await getNewsByCategoryId("01");
+    const breakingNewsArrOfObj = breakingNews.data;
+ 
+
     return (
-        <div className="bg-amber-200 flex p-2 gap-3" >
+        <div className="bg-red-200 flex p-2 gap-3" >
                  <button className="btn btn-error">Latest</button>
           <Marquee pauseOnHover={true}>
-       
-           <p> jabir bhai jabir bhai jabir bhai jabir bhai</p>
+       <div className="flex gap-5">
+       {
+        breakingNewsArrOfObj.map((x)=><p key={x._id}>{x.title}!</p>)
+       }
+       </div>
           </Marquee>
         </div>
     );
