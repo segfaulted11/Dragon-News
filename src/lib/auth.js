e73554c4a879f3b617f20b7f13ab4ecde12854f7
Seg Fault <segfaulted11@gmail.com>
2026-06-21 13:12:15 +0600

    .env file added

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,8 +1,9 @@
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
 const client = new MongoClient(process.env.MONGO_URI);
-console.log("MONGO_URI:", process.env.MONGO_URI);
+
 const db = client.db();
 export const auth = betterAuth({
   database: mongodbAdapter(db, {
